6e34bf6d5927da49b7f8e384014bc1089727c817
dfisch15 <dennis@example.com>
2023-12-10 20:27:49 -0500

    troubleshooting~

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   const msg = 'Hello World 2!\n'
-  const message = process.env.NODE_ENV === 'preview' ? 'Hello World Preview' : 'Hello World Active';
   res.end(msg);
 });
 
